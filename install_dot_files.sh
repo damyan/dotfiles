ba5f186975a3c040bb2c10d3e515749512e07820
Damyan Yordanov <damyan+dev@0x4711.org>
2020-12-28 21:27:23 +0100

    Add install script   -- backup and create new .tmux.conf   -- backup and create new .bashrc   -- backup and create new .vim/vimrc

---
+++ install_dot_files.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+# debug only
+#set -x
+#trap read debug
+#
+suffix=$(date +%Y-%m-%d-%H-%M)
+dot_files_folder="${HOME}/.dotfiles"
+
+
+#################### vim ####################
+vim_folder=.vim
+vim_config_file=vimrc
+vim_config_path="${vim_folder}/${vim_config_file}"
+
+if [ -d "${HOME}/${vim_folder}" ]; then
+  if [ -f "${HOME}/${vim_config_path}" ]; then
+    mv ${HOME}/${vim_config_path} ${HOME}/${vim_config_path}-${suffix}
+  fi
+else
+  mkdir -p "${HOME}/${vim_folder}"
+fi
+
+cp ${dot_files_folder}/${vim_config_path} ${HOME}/${vim_config_path}
+#################### vim ####################
+
+
+#################### tmux ####################
+tmux_config_path=.tmux.conf
+
+if [ -f "${HOME}/${tmux_config_path}" ]; then
+  mv ${HOME}/${tmux_config_path} ${HOME}/${tmux_config_path}-${suffix}
+fi
+cp ${dot_files_folder}/${tmux_config_path} ${HOME}/${tmux_config_path}
+#################### tmux ####################
+
+
+#################### BASH ####################
+bash_config_path=.bashrc
+
+if [ -f "${HOME}/${bash_config_path}" ]; then
+  cp ${HOME}/${bash_config_path} ${HOME}/${bash_config_path}-${suffix}
+fi
+cat ${dot_files_folder}/${bash_config_path} >> ${HOME}/${bash_config_path}
+#################### BASH ####################
